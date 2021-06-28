f4c5b8b08c72117c64b4cb0f35b8f58878625fe7
richbruzzese <85512661+richbruzzese@users.noreply.github.com>
2021-06-28 10:26:36 -0400

    Color Selections
    - added variables to select the design and color elements from the page
    - added listener to determine when a change is made to the design selection
    - created a loop to run through the options comparing it to the design choice and omitting color choices for shirts that don't match the selection

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-const name = document.getElementById('name')
+const nameField = document.getElementById('name')
+nameField.focus();
+
 const jobRole = document.getElementById('title')
 const otherJob = document.getElementById('other-job-role')
 otherJob.style.visibility = "hidden" 
+
 const tshirtDesign = document.getElementById('design')
-const tshirtColor = document.getElementById=('color')
-name.focus();
-console.log(tshirtColor).children
-// tshirtColor.children.style.visibility="hidden"
+const tshirtColor = document.getElementById('color')
+const colorOptions = tshirtColor.children
+
+tshirtColor.disabled = true
+
 jobRole.addEventListener("change", (e) =>{
     if(e.target.value === "other"){
 otherJob.style.visibility = "visible"
     }else{otherJob.style.visibility="hidden"
 
+    }
+})
+tshirtDesign.addEventListener('change', (e) =>{
+    tshirtColor.disabled = false
+    let designSelection = e.target.value
+    for(let i=0; i<colorOptions.length; i++){
+       let colorSelection = colorOptions[i].getAttribute("data-theme")
+        if(designSelection == colorSelection){
+            colorOptions[i].hidden = false;
+            colorOptions[i].setAttribute('selected', true)
+        }else{
+            colorOptions[i].hidden = true;
+            colorOptions[i].removeAttribute('selected', false)
+        }
     }
 })
