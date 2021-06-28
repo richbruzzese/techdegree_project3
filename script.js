2253cb45270932d2c93bad7a701c5e800c10405b
richbruzzese <85512661+richbruzzese@users.noreply.github.com>
2021-06-28 11:52:52 -0400

    Additional functionality
    - added functionality to add registration costs together when selecting a course
    - added functionality for payment type to auto select credit card
    - toggle div elements based on the selection
    - beginning work on form validation

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const nameField = document.getElementById('name')
-nameField.focus();
+    nameField.focus();
 
 const jobRole = document.getElementById('title')
 const otherJob = document.getElementById('other-job-role')
-otherJob.style.visibility = "hidden" 
+    otherJob.style.visibility = "hidden" 
 
 const tshirtDesign = document.getElementById('design')
 const tshirtColor = document.getElementById('color')
 const colorOptions = tshirtColor.children
 
-tshirtColor.disabled = true
+    tshirtColor.disabled = true
+
+const registration = document.getElementById('activities')
+const activitiesCost = document.getElementById('activities-cost')
+let totalCost = 0
+
+const paymentSelection = document.getElementById('payment')
+    paymentSelection.children[1].setAttribute('selected', true)
+const creditCard = document.getElementById('credit-card')
+const paypal = document.getElementById('paypal')
+    paypal.hidden = true;
+const bitcoin = document.getElementById('bitcoin')
+    bitcoin.hidden = true;
 
 jobRole.addEventListener("change", (e) =>{
     if(e.target.value === "other"){
@@ -18,6 +30,7 @@ otherJob.style.visibility = "visible"
 
     }
 })
+
 tshirtDesign.addEventListener('change', (e) =>{
     tshirtColor.disabled = false
     let designSelection = e.target.value
@@ -30,5 +43,36 @@ tshirtDesign.addEventListener('change', (e) =>{
             colorOptions[i].hidden = true;
             colorOptions[i].removeAttribute('selected', false)
         }
+    }
+})
+
+registration.addEventListener('change', (e) =>{
+    let price = e.target.getAttribute("data-cost") 
+    price = +price
+    if(e.target.checked == true){
+        totalCost = totalCost + price
+    }else if(e.target.checked == false){
+        totalCost = totalCost - price
+    }
+    activitiesCost.innerHTML= `Total: $${totalCost}`
+})
+
+paymentSelection.addEventListener('change', (e)=>{
+    switch(e.target.value){
+        case 'credit-card':
+            creditCard.hidden = false;
+            paypal.hidden = true
+            bitcoin.hidden = true
+            break;
+        case 'paypal':
+            paypal.hidden = false
+            creditCard.hidden = true
+            bitcoin.hidden = true
+            break;
+        case 'bitcoin':
+            bitcoin.hidden = false
+            creditCard.hidden = true
+            paypal.hidden = true
+
     }
 })
